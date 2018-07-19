40e39f2c51ada4dfb345b29e0dc337436d7cf8c3
Rakesh Sharma <rakeshsharma.sharma@gmail.com>
2018-07-19 10:58:13 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IData } from './data.interface';
 import { DataService } from './data.service';
-
+// http://www.palador.com/2017/02/28/create-a-pie-chart-with-dynamic-data-using-d3-js-angular-2/
+//https://github.com/amanjain325/angular-d3-charts/tree/master/src/app
+//donut d3 chart examplewith angular 6
 @Component({
   selector: 'my-app',
   templateUrl: '/app/app.html'
